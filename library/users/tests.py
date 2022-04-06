1f4fb5e83ac74418f68016fff98cd2d72561ad9e
Shadow48lip <shome@lipsat.ru>
2022-04-06 22:03:39 +0300

    Add lesson_8 Add tests for User model

--- library/users/tests.py
+++ library/users/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from mixer.backend.django import mixer
+from .models import User
 
-# Create your tests here.
+
+class TestTodoViewSet(APITestCase):
+    def test_get_list(self):
+        user = User.objects.create_superuser('admin', 'admin@geekbrains.local', 'admin123')
+        self.client.login(username='admin', password='admin123')
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_detail(self):
+        user = User.objects.create_superuser('admin', 'admin@geekbrains.local', 'admin123')
+        self.client.login(username='admin', password='admin123')
+
+        user_item = mixer.blend(User)
+        # response = self.client.get(f'/api/users/{user_item.id}/')
+        response = self.client.put(f'/api/users/{user_item.id}/',
+                                   {'username': 'ivan', 'password': user_item.password, 'email': 'user@mail.ru'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user_item = User.objects.get(id=user_item.id)
+        self.assertEqual(user_item.username, 'ivan')
